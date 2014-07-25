f92a125da559085e94bd9675fdb332c7f88b73ff
Guy Halperin & Sergii Mertsalov <student@epicodus.com>
2014-07-25 14:01:18 -0700

    add logic for 4 test

--- lib/numbers_in_words.rb
+++ lib/numbers_in_words.rb
@@ -1,8 +1,66 @@
 def numbers_in_words(number)
-  words = { 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine' }
-  words.each do |k,v|
-    return v if number == k
-  end
+  words = { '1' => 'one', '2' => 'two', '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six', '7' => 'seven', '8' => 'eight', '9' => 'nine', '10' => 'ten', '11' => 'eleven',
+            '12' => 'twelve', '13' => 'thirteen', '14' => 'fourteen', '15' => 'fifteen', '16' => 'sixteen', '17' => 'seventeen', '18' => 'eighteen', '19' => 'nineteen',
+            '20' => 'twenty', '30' => 'thirty', '40' => 'forty', '50' => 'fifty', '60' => 'sixty', '70' => 'seventy', '80' => 'eighty', '90' => 'ninety'
+          }
+
+
+
+  tens = words[(number.to_i - (number.to_i % 10)).to_s]
+  singles = words[(number.to_i % 10).to_s]
+  tens +' '+ singles
 end
 
-puts numbers_in_words(9)
+p numbers_in_words('78')
+
+
+
+
+
+
+
+
+
+
+
+#   new_number = number
+#   tens_digit = ''
+#   last_digit = ''
+#   array = []
+
+#  puts words[number]
+#   words.each do |k,v|
+#     if number.length == 3
+#       hundreds_digit = number.slice(0)
+#       if hundreds_digit.eql?(k)
+#         array << v + " hundred"
+#         # hundreds = v + "hundred"
+#       end
+#       new_number = number.slice(-2..-1)
+#     end
+#   end
+
+#  #loop through number.length
+#  #if words[number]
+#  #array << words number
+#  #number -= number
+
+
+#   words.each do |k,v|
+#     if new_number.eql?(k)
+#         puts number
+#         array << v
+#     elsif new_number.to_i > 19 && new_number.to_i <= 99 && new_number[1] != "0"
+#       tens_digit = new_number.slice(0) + "0"
+#       last_digit = new_number.slice(-1)
+#       if tens_digit.eql?(k)
+#         array.push(v)
+#       elsif last_digit.eql?(k)
+#         array << v
+#       end
+#     end
+#   end
+#    array.join(" ")
+# end
+
+#  numbers_in_words('525')
--- spec/numbers_in_words_spec.rb
+++ spec/numbers_in_words_spec.rb
@@ -2,7 +2,23 @@ require('rspec')
 require('numbers_in_words')
 
 describe('numbers_in_words') do
-  it('return word for digit from 0 to 9') do
-    numbers_in_words(9).should(eq('nine'))
+  it('return a word for digit from 0 to 9') do
+    numbers_in_words('9').should(eq('nine'))
+  end
+
+  it('return a word for digits from 10 to 19') do
+    numbers_in_words('13').should(eq('thirteen'))
+  end
+
+  it('return the multiples of ten under one hundred') do
+    numbers_in_words('70').should(eq('seventy'))
+  end
+
+  it('handles a combination of a tens and single number') do
+    numbers_in_words('57').should(eq('fifty seven'))
+  end
+
+  it('handles numbers up to 999') do
+    numbers_in_words('843').should(eq('eight hundred forty three'))
   end
 end
